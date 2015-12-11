6fe9401fc8d8e541d2efd5385a993503ad60cb20
Przemek Karbownik <przekar4926@gmail.com>
2015-12-11 13:01:16 +0100

    creation of LAG_atmega

---
+++ LAG_PROJECT/LAG_atmega/LAG_atmega/LAG_atmega.c
@@ -0,0 +1,17 @@
+/*
+ * LAG_atmega.c
+ *
+ * Created: 2015-12-11 12:57:46
+ *  Author: Przemek
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
